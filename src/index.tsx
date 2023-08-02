d5b8e912030dba4c8abd2705f4a36e2522be6b1f
Dzane <dzane@student.21-school.ru>
2023-08-02 16:30:34 +0700

    feat: styles, libs

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.scss'
+import { App } from 'app/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
